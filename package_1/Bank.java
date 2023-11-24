83986cca6c1a0e8a93a6b3ba7cc103825bbb78f6
fizzah26 <SP23-BCS-048@cuilahore.edu.pk>
2023-11-24 14:02:59 +0500

    bank management

---
+++ package_1/Bank.java
@@ -0,0 +1,64 @@
+package package_1;
+
+public class Bank {
+    public class Bank implements BankFunctions {
+        private BankAccount[] account = new BankAccount[1000];
+        private AccountHolder[] accountHolder = new AccountHolder[1000];
+
+        int c = 1;
+
+
+        public void createAccount(BankAccount accounts,AccountHolder accountHolders) {
+            for (int i = 0; i < account.length; i++){
+                if(account[i]==null){
+                    account[i]=accounts;
+                    accountHolder[i]=accountHolders;
+                    c++;
+                    System.out.println("Account created successfully");
+                    return;
+                }
+            }
+        }
+        public void depositCash(int accountNumber , double amount) {
+            for (int i = 0; i < account.length; i++) {
+                if (account[i] != null && account[i].getAccountNumber() == accountNumber) {
+                    account[i].setBalance(account[i].getBalance() + amount);
+                    System.out.println("Cash deposited successfully");
+                    return;
+                }
+            }
+            System.out.println("Account not found!!");
+        }
+
+        public void withdrawCash(int accountNumber ,double amount) {
+            for (int i = 0; i < account.length; i++) {
+                if (account[i] != null && account[i].getAccountNumber() == accountNumber) {
+                    if (amount <= account[i].getBalance()) {
+                        account[i].setBalance(account[i].getBalance() - amount);
+                        System.out.println("cash withdrawed successfully");
+                    } else
+                        System.out.println("insufficient Balance!!");
+                    return;
+                }
+            }
+            System.out.println("Account not found!!");
+        }
+
+        public void displayAccountInfo(int accountNumber) {
+            for (int i = 0; i < account.length; i++) {
+                if (account[i] != null && account[i].getAccountNumber() == accountNumber) {
+                    System.out.println("Account Information:");
+                    System.out.println("Account Number: " + account[i].getAccountNumber());
+                    System.out.println("Account Type: " + account[i].getAccountType());
+                    System.out.println("Balance: " + account[i].getBalance());
+                    System.out.println("\nAccount Holder Information:");
+                    System.out.println("Name: " + accountHolder[i].getName());
+                    System.out.println("Address: " + accountHolder[i].getAddress());
+                    System.out.println("Phone Number: " + accountHolder[i].getPhoneNumber());
+                    return;
+                }
+            }
+        }
+    }
+
+}
---
+++ package_1/BankAccount.java
@@ -0,0 +1,42 @@
+package package_1;
+
+    public class BankAccount {
+        private  int accountNumber=0;
+        private AccountType accountType;
+        double balance=0;
+
+        public BankAccount( int accountNumber,AccountType accountType, double balance) {
+            this.accountNumber = accountNumber;
+            this.accountType = accountType;
+            this.balance = balance;
+        }
+
+        public int getAccountNumber() {
+            return accountNumber;
+        }
+
+        public void setAccountNumber(int accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public AccountType getAccountType() {
+            return accountType;
+        }
+
+        public void setAccountType(AccountType accountType) {
+            this.accountType = accountType;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+        public String toString(){
+            return String.format("%d,%s,%f",accountNumber,accountType,balance);
+        }
+    }
+
+}
